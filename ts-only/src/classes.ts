c018c05d2b7a4e66def816cd350be95812c0c0d1
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-27 15:25:47 +0100

    classes-2

--- ts-only/src/classes.ts
+++ ts-only/src/classes.ts
@@ -1,29 +1,41 @@
 export class Animal {
-  name: string;
-  protected anyVariable: string;
-
-  constructor(name: string) {
-    this.name = name
-    this.anyVariable = "";
+  constructor(
+    public name: string,
+    protected anyVariable = ""
+  ) {
   }
 }
 
 class Dog extends Animal {
-  private owner: string;
-
-  constructor(name: string, owner: string) {
-    super(name)
-    this.owner = owner
-    this.anyVariable = "test"
+  constructor(
+    name: string,
+    private owner: string
+  ) {
+    super(name, "test")
   }
 
   hasSameOwner(dog: Dog) {
     return dog.owner === this.owner;
   }
+
+  static speak(dog: Dog) {
+    return `${dog.name}, ${dog.owner}'s dog, says hau`;
+  }
+
+  get isHappy() {
+    return Math.random() < 0.5
+  }
+
+  set isHappy(value: boolean) {
+    console.log('pfff')
+  }
 }
 
 let dog = new Dog('Fluffy', 'Piotr')
 
+console.log(dog.isHappy)
+dog.isHappy = true
+console.log(Dog.speak(dog))
 console.log(dog instanceof Dog)
 console.log(dog instanceof Animal)
 console.log(dog instanceof Array)

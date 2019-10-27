2ba677757a82036f3a52d79f96d5810b61f847e6
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-27 10:46:06 +0100

    przekazywanie 2 argumentow

--- js-to-ts/src/hello.ts
+++ js-to-ts/src/hello.ts
@@ -2,9 +2,15 @@ function sayHello (person: string) {
   console.log('Hello ' + person)
 }
 
+function getFullName (person: {firstName: string, lastName: string}): string {
+  return person.firstName + ' ' + person.lastName
+}
+
 let me = {
   firstName: 'Kasia',
   lastName: 'Izak'
 }
 
-sayHello(me.firstName)
+let fullName = getFullName(me)
+
+sayHello(fullName)
